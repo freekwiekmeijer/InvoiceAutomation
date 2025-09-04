e2940bb1acd6315305b0afa2b30928a36dbfd7b9
Freek Wiekmeijer <info@rbiv.nl>
2025-09-04 09:29:44 +0200

    Add dependency injection

--- InvoiceAutomation.ConsoleHost/Program.cs
+++ InvoiceAutomation.ConsoleHost/Program.cs
@@ -1,20 +1,34 @@
 ﻿namespace InvoiceAutomation.ConsoleHost;
 
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
 using InvoiceAutomation.Service;
+using InvoiceAutomation.Service.Interfaces;
 using InvoiceAutomation.Service.Models;
-
-
 
 public class Program
 {
+    private static IHost AppHost { get; set; }
+
     static void Main(string[] args)
     {
+        SetupDependencyInjection();
         CreateInvoice();
+    }
+
+    private static void SetupDependencyInjection()
+    {
+        AppHost = Host.CreateDefaultBuilder()
+            .ConfigureServices(services =>
+            {
+                services.AddTransient<IPdfCreator, PdfCreator>();
+            })
+            .Build();
     }
 
     private static void CreateInvoice()
     {
-        var pdfCreator = new PdfCreator();      // TODO : Use DI
+        var pdfCreator = AppHost.Services.GetRequiredService<IPdfCreator>();
 
         using var documentStream = pdfCreator.CreateInvoice(new List<InvoiceLine>());
 
